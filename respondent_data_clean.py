59902f4d15fe5d24e586a9653c924a56306b398a
Collin Clifford <Collin.G.Clifford@gmail.com>
2022-03-08 21:11:52 +0000

    5

--- respondent_data_clean.py
+++ respondent_data_clean.py
@@ -5,4 +5,3 @@ def join_respondents(contact_info_file, other_info_file, output_file):
     respondents = res_con.join(res_oth, on="respondent_id")
     respondents['birthdate'] = respondents['birthdate'].to_datetime(format='%Y-%m-%d')
     return respondents.to_csv(output_file)
-#join_respondents
